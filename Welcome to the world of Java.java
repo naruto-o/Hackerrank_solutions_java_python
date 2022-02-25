ff3910a8decf271ffd1a75d46242c0c883b7ac81
shikhar sharma <73755581+naruto-o@users.noreply.github.com>
2022-02-25 17:40:31 +0530

    Update Welcome to the world of Java.java

--- Welcome to the world of Java.java
+++ Welcome to the world of Java.java
@@ -1,5 +1,6 @@
 public class Solution {
-
+// https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+//     link for this challenge
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
