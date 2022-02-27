ff7e94b3c55b0d920921af2206874906940d4271
shikhar sharma <73755581+naruto-o@users.noreply.github.com>
2022-02-27 23:37:40 +0530

    Create java-stdin-and-stdout-1.java

---
+++ java-stdin-and-stdout-1.java
@@ -0,0 +1,19 @@
+// link for the question
+// https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a);
+        int b = scan.nextInt();
+        System.out.println(b);
+        int c = scan.nextInt();
+        System.out.println(c);
+        // Complete this line
+        
+        // Complete this line
+    }
+}
