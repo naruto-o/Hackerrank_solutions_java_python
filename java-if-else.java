beeb6cc928f7272b4090cefc4ae802b9b2880881
shikhar sharma <73755581+naruto-o@users.noreply.github.com>
2022-02-28 20:32:32 +0530

    Create java-if-else.java

---
+++ java-if-else.java
@@ -0,0 +1,27 @@
+// https://www.hackerrank.com/challenges/java-if-else/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+//link of the challenge given above
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        /* Read input */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+        /* Determine answer */
+        String ans = "";
+        if (n % 2 == 1) {
+            ans = "Weird";
+        } else {
+            if (n >= 6 && n <= 20) {
+                ans = "Weird";
+            } else {
+                ans = "Not Weird";
+            }
+        }
+        
+        /* Print output */
+        System.out.println(ans);
+    }
+}
