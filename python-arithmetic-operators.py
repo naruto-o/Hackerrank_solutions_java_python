d3b12cc3e83537573d5e03c9d28edb7f81f842b8
shikhar sharma <73755581+naruto-o@users.noreply.github.com>
2022-03-01 17:34:58 +0530

    Create python-arithmetic-operators.py

---
+++ python-arithmetic-operators.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+# link for above challenge
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
