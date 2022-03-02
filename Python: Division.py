b45089c8f5484718bc344efa67db1c62e5df4f88
shikhar sharma <73755581+naruto-o@users.noreply.github.com>
2022-03-02 14:47:59 +0530

    Create Python: Division.py

---
+++ Python: Division.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+# link for the above challenge
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+
+o1 = a // b
+o2 = a/b
+print(o1)
+print(o2)
